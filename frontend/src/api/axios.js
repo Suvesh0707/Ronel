6edb9d1bee0c70ba8f1b26d3ce8f4bb1223f176a
suvesh0707 <suveshpagam07@gmail.com>
2026-03-01 19:41:34 +0530

    frontend changed api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 // src/api/axios.js
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const baseURL = import.meta.env.VITE_API_URL || "http://51.20.18.167/api";
 
 const instance = axios.create({
   baseURL,
